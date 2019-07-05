16789355729313cb0205ff805333f1c1ef21c3bc
Zubair <zubair.maqsood6@gmail.com>
2019-07-05 17:52:57 +0100

    Zubair: Created ordered buttons for dates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import EventsPage from './components/EventsPage.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
        <h1>TimeSkip</h1>
+      <EventsPage />
       </header>
     </div>
   );
---
+++ src/components/EventsPage.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import TimelineContainer from './Timeline/TimelineContainer.js'
+
+const eventsURL = "http://localhost:3000/events"
+
+class EventsPage extends React.Component {
+	state = {
+		events: []
+	}
+
+	componentDidMount() {
+		fetch(eventsURL)
+		.then((resp) => resp.json())
+		.then((data) => this.setState({events: data}))
+	}
+
+	sortedEvents = () => {
+		const eventsArray = [...this.state.events]
+
+		//sorts all of the events based on the conditions below
+		return eventsArray.sort((event1, event2)=>{
+
+		//bubble sort algorithm
+			const event1date = parseInt(event1.date.slice(0,4),10)
+			const event2date = parseInt(event2.date.slice(0,4),10)
+			return event1date < event2date ? -1 : 0
+		})
+	}
+
+	render() {
+		return(
+		<div>
+			<TimelineContainer events={this.sortedEvents()}/>
+		</div>
+
+		)
+	}
+}
+
+export default EventsPage
