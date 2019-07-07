87bafdb0c1506760cfa4cc9e44af29214fffdecc
Ayleen Nazario <ayleen.nazario@gmail.com>
2019-07-07 02:39:59 +0100

    Topic page now iteractes over all topics to display a topic card. The topic card for space links to /space-timeline and displays only space events

--- src/components/Timeline/TimelineContainer.js
+++ src/components/Timeline/TimelineContainer.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import TimelineCard from './TimelineCard.js'
+import TimelineCard_Space from './TimelineCard_Space'
 
 class TimelineContainer extends React.Component {
+	
 	render() {
 		return (
 			<div>
-				{this.props.events.map((event, index) => <TimelineCard key={index} date={event.date} />)}
+				
+				{ this.props.events.map((event, index) =>  <TimelineCard_Space key={index} date={event.date} />)}
 
 			</div>
 		)
