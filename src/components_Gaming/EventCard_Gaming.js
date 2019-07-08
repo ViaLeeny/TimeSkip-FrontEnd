ded87a257f2b3ac7706caf43b00d03e3257fae9d
Ayleen Nazario <ayleen.nazario@gmail.com>
2019-07-09 00:07:58 +0100

    Implemented comments on gaming timeline

--- src/components_Gaming/EventCard_Gaming.js
+++ src/components_Gaming/EventCard_Gaming.js
@@ -1,9 +1,37 @@
 import React from 'react'
 import { Segment, Header, Image, Icon } from 'semantic-ui-react'
+import ContributionContainer_Gaming from "./Timeline_Gaming/Contributions_Gaming/ContributionsContainer_Gaming"
 
+
+const CONTRIBUTIONS_URL = `http://localhost:3000/contributions/`
 
 class EventCard_Gaming extends React.Component {
+  //SETTING STATE
+  state = {
+    contributions: [],
+    filteredContributions: []
+  }
 
+  //FETCH CONTRIBUTIONS 
+  componentDidMount() {
+    return fetch(CONTRIBUTIONS_URL)
+      .then(resp => resp.json())
+      .then(contributions => {
+        this.setState({ contributions });
+      });
+  }
+
+  //FILTER CONTRIBUTIONS FOR THIS EVENT
+  filterContributions = () => {
+    //if (this.state.contributions.length > 0) {
+      let eventContributions = [...this.state.contributions].filter(
+        contribution => contribution.event_id === this.props.selectedEvent.id
+      );
+      console.log("Here are the filtered contributions for this event:");
+      console.log(eventContributions);
+ 
+      return eventContributions;
+  };
 
 	render() {
 		return (
@@ -19,7 +47,12 @@ class EventCard_Gaming extends React.Component {
               <Icon name='add' />
               <Icon name='heart' />
             </Segment>
-            
+            <ContributionContainer_Gaming
+
+              event={this.props.event}
+              contributions={this.filterContributions()}
+            />
+       
           </div>
 		)
 	}
